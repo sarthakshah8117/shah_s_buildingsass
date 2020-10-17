e6aba745547b4596f2693a81a0132e45ecde9f15
Sarthak Shah <sharthakshah81173@gmail.com>
2020-10-17 02:32:46 -0400

    update

--- js/main.js
+++ js/main.js
@@ -6,22 +6,28 @@
     .then(data => {
 
         debugger;
-    
+        
+        // handle data
         handleData(data);
 
-})
-function handleData(data)
-{
-    let courseName = document.querySelector('.profPanelText .text-muted'),
-        profname = document.querySelector('.profPanelText .profName'),
-        time = document.querySelector('.profPanelText .list-inline');
+    })
 
-        courseName.innerHTML = data.coursename + "-" +'<span class="text-primary">' + data.coursecode +"</span>";
-        // profname.innerHTML = "Professor -"+data.profname;
-        // profname.innerHTML = "Professor" + data.profname;
-        time.innerHTML = "<li>" + "&#128338;" + data.classtime[0] + "</li>" + "<li>" + "&#128338;" + data.classtime[1] + "</li>"
 
-}
+    function handleData(data)
+    {
+        let courseName = document.querySelector('.profPanelText .text-muted'),
+            profname = document.querySelector('.profPanelText .profName'),
+            time = document.querySelector('.profPanelText .list-inline');
+            
+            // Display text
+            courseName.innerHTML = data.coursename + "-" +'<span class="text-primary">' + data.coursecode +"</span>";
+            
+            // display time
+            time.innerHTML = "<li>" + "&#128338;" + data.classtime[0] + "</li>" + "<li>" + "&#128338;" + data.classtime[1] + "</li>"
+            
+            // display name
+            profname.innerHTML = "Professor - " + data.profname;
+    }
 
 
 })();
